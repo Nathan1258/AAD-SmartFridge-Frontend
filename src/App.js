d0a2cb4368ce1f632b00aeb8e0e155e340a3972e
Nathan <N1043109@my.ntu.ac.uk>
2023-12-16 16:11:29 +0000

    changed font

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
+import {useEffect} from "react";
+import WebFont from "webfontloader";
 
 const AppContainer = styled.div
 `
   display: flex;
   height: 100vh;
+  font-family: 'Roboto';
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -11,7 +14,17 @@ const AppContainer = styled.div
 
 const Title = styled.h1``;
 
+
 function App() {
+
+     useEffect(() => {
+   WebFont.load({
+     google: {
+       families: ['Roboto']
+     }
+   });
+  }, []);
+
   return (
       <AppContainer>
           <Title>Smart Fridge</Title>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
+import WebFont from 'webfontloader';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
     <App />
