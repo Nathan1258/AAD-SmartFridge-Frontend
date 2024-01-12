613f7f6e767d67fcc49db1203bc5460393972ad2
Nathan <N1043109@my.ntu.ac.uk>
2024-01-12 11:56:04 +0000

    Revert "All current front-end files added"
    This reverts commit 465ab4c752907fc6a35c3b61462c5d8eefabb7a4.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from "react";
-import LoginPage from "./pages/LoginPage";
-import HomePage from "./pages/HomePage"
+import styled from "styled-components";
+import {useEffect} from "react";
+import WebFont from "webfontloader";
+
+const AppContainer = styled.div
+`
+  display: flex;
+  height: 100vh;
+  font-family: 'Roboto';
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1``;
+
 
 function App() {
+
+     useEffect(() => {
+   WebFont.load({
+     google: {
+       families: ['Roboto']
+     }
+   });
+  }, []);
+
   return (
-    <>
-    <HomePage/>
-    </>
+      <AppContainer>
+          <Title>Smart Fridge</Title>
+      </AppContainer>
   );
 }
 
