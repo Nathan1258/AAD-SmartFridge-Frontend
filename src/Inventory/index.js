635bb37be6f361b28c08c306248f192b1bf0447b
Nathan <nathan@ellisn.com>
2024-01-23 16:51:45 +0000

    added "Items in stock" view

--- src/Inventory/index.js
+++ src/Inventory/index.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import { useEffect, useState } from "react";
 import Loading from "../ReuseableComponents/Loading";
-import { getAllItems } from "../API";
+import { getAllItems, getAllItemsInStock } from "../API";
 import { usePopup } from "../Popup/popupContext";
 import Input from "../ReuseableComponents/Input";
 import { TiArrowSortedDown, TiArrowSortedUp } from "react-icons/ti";
+import DropdownSelector from "../ReuseableComponents/DropdownSelector";
 
 const InventoryWrapper = styled.div`
   display: flex;
@@ -23,7 +24,6 @@ const SubTitle = styled.h4`
   padding-top: 10px;
   color: white;
 `;
-
 const MainContent = styled.div`
   display: flex;
   justify-content: center;
@@ -32,11 +32,12 @@ const MainContent = styled.div`
   overflow: auto;
   padding-top: 25px;
 `;
-
 const FilterOptions = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
   padding-top: 20px;
 `;
-
 const Table = styled.table`
   width: 100%;
   border-collapse: collapse;
@@ -79,7 +80,6 @@ const Table = styled.table`
     padding: 15px;
   }
 `;
-
 const TableContainer = styled.div`
   width: 100%;
   overflow-x: auto;
@@ -91,6 +91,7 @@ function itemsTable(
   sortOrder,
   handleHeaderClick,
   isLoading,
+  selectedView,
 ) {
   return (
     <>
@@ -105,32 +106,74 @@ function itemsTable(
               <Table>
                 <thead>
                   <tr className="header">
-                    <th
-                      className={"firstHeader"}
-                      onClick={() => handleHeaderClick("name")}
-                    >
-                      Product Name{" "}
-                      {lastClickedHeader === "name" && sortOrder === "asc" ? (
-                        <TiArrowSortedDown />
-                      ) : (
-                        <TiArrowSortedUp />
-                      )}
-                    </th>
-                    <th onClick={() => handleHeaderClick("price")}>
-                      Product Price{" "}
-                      {lastClickedHeader === "price" && sortOrder === "asc" ? (
-                        <TiArrowSortedDown />
-                      ) : (
-                        <TiArrowSortedUp />
-                      )}
-                    </th>
+                    {selectedView === "Items in stock" ? (
+                      <>
+                        <th
+                          className={"firstHeader"}
+                          onClick={() => handleHeaderClick("name")}
+                        >
+                          Product Name{" "}
+                          {lastClickedHeader === "name" &&
+                          sortOrder === "asc" ? (
+                            <TiArrowSortedDown />
+                          ) : (
+                            <TiArrowSortedUp />
+                          )}
+                        </th>
+                        <th>Quantity</th>
+                        <th>Expiry Date</th>
+                        <th onClick={() => handleHeaderClick("price")}>
+                          Product Price{" "}
+                          {lastClickedHeader === "price" &&
+                          sortOrder === "asc" ? (
+                            <TiArrowSortedDown />
+                          ) : (
+                            <TiArrowSortedUp />
+                          )}
+                        </th>
+                        <th>Last Updated</th>
+                      </>
+                    ) : (
+                      <>
+                        <th
+                          className={"firstHeader"}
+                          onClick={() => handleHeaderClick("name")}
+                        >
+                          Product Name{" "}
+                          {lastClickedHeader === "name" &&
+                          sortOrder === "asc" ? (
+                            <TiArrowSortedDown />
+                          ) : (
+                            <TiArrowSortedUp />
+                          )}
+                        </th>
+                        <th onClick={() => handleHeaderClick("price")}>
+                          Product Price{" "}
+                          {lastClickedHeader === "price" &&
+                          sortOrder === "asc" ? (
+                            <TiArrowSortedDown />
+                          ) : (
+                            <TiArrowSortedUp />
+                          )}
+                        </th>
+                      </>
+                    )}
                   </tr>
                 </thead>
                 <tbody>
                   {filteredItems.map((item) => (
                     <tr className={"itemRow"} key={item.productID}>
                       <td>{item.Name}</td>
+                      {selectedView === "Items in stock" && (
+                        <>
+                          <td>{item.quantity}</td>
+                          <td>{formatDateToReadable(item.expiryDate)}</td>
+                        </>
+                      )}
                       <td>{item.Price}</td>
+                      {selectedView === "Items in stock" && (
+                        <td>{formatDateToReadable(item.lastUpdated)}</td>
+                      )}
                     </tr>
                   ))}
                 </tbody>
@@ -151,36 +194,53 @@ export function Inventory(props) {
   const [filteredItems, setFilteredItems] = useState([]);
   const [sortOrder, setSortOrder] = useState("asc");
   const [lastClickedHeader, setLastClickedHeader] = useState(null);
+  const [selectedView, setSelectedView] = useState("All items");
 
   useEffect(() => {
-    getAllItems()
-      .then((items) => {
-        setItems(items);
-        console.log(items[0]);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching items:", error);
-        triggerPopup(
-          ":(",
-          "We ran into an error fetching products. Please refresh the page and try again.",
-          "Okay",
-          () => {
-            window.location.reload();
-          },
-        );
-      });
-  }, []);
+    if (selectedView === "All items") {
+      getAllItems()
+        .then((items) => {
+          setItems(items);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching items:", error);
+          triggerPopup(
+            ":(",
+            "We ran into an error fetching products. Please refresh the page and try again.",
+            "Okay",
+            () => {
+              window.location.reload();
+            },
+          );
+        });
+    } else if (selectedView === "Items in stock") {
+      getAllItemsInStock()
+        .then((items) => {
+          setItems(items);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching items:", error);
+          triggerPopup(
+            ":(",
+            "We ran into an error fetching products. Please refresh the page and try again.",
+            "Okay",
+            () => {
+              window.location.reload();
+            },
+          );
+        });
+    }
+  }, [selectedView]);
 
   useEffect(() => {
     setLastClickedHeader("name");
   }, []);
-
   useEffect(() => {
     const filtered = items.filter((item) =>
       item.Name.toLowerCase().includes(productSearchQuery.toLowerCase()),
     );
-
     // Sort by Product Name
     const sortedByName = [...filtered].sort((a, b) => {
       const nameA = a.Name.toLowerCase();
@@ -200,8 +260,7 @@ export function Inventory(props) {
     setFilteredItems(
       lastClickedHeader === "name" ? sortedByName : sortedByPrice,
     );
-  }, [productSearchQuery, items, sortOrder]);
-
+  }, [productSearchQuery, items, sortOrder, selectedView]);
   const toggleSortOrder = () => {
     setSortOrder(sortOrder === "asc" ? "desc" : "asc");
   };
@@ -211,12 +270,22 @@ export function Inventory(props) {
     toggleSortOrder();
   };
 
+  const handleViewChange = (event) => {
+    setSelectedView(event.target.value);
+  };
+
   return (
     <InventoryWrapper>
       <Title>Inventory</Title>
       <SubTitle>Manage and View Inventory Stock</SubTitle>
 
       <FilterOptions>
+        <DropdownSelector
+          options={["All items", "Items in stock"]}
+          selectedOption={selectedView}
+          onChange={handleViewChange}
+          height={"50px"}
+        />
         <Input
           placeholder={"Search for a product"}
           value={productSearchQuery}
@@ -231,8 +300,52 @@ export function Inventory(props) {
           sortOrder,
           handleHeaderClick,
           isLoading,
+          selectedView,
         )}
       </MainContent>
     </InventoryWrapper>
   );
+}
+
+function formatDateToReadable(inputDate) {
+  const date = new Date(inputDate);
+
+  const day = date.getDate();
+  const monthIndex = date.getMonth();
+  const year = date.getFullYear();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const daySuffix = getDaySuffix(day);
+  const formattedDate = `${day}${daySuffix}  ${months[monthIndex]}  ${year}`;
+  return `${formattedDate}`;
+}
+
+function getDaySuffix(day) {
+  if (day >= 11 && day <= 13) {
+    return "th";
+  }
+  switch (day % 10) {
+    case 1:
+      return "st";
+    case 2:
+      return "nd";
+    case 3:
+      return "rd";
+    default:
+      return "th";
+  }
 }
