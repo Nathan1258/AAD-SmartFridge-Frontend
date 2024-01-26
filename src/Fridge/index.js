f578a0e21c50734e57798023d418979e76f3c882
matty <mattysiviter7@gmail.com>
2024-01-26 10:46:41 +0000

    The table in fridge tab can now access the backend and database infomation

--- src/Fridge/index.js
+++ src/Fridge/index.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import Button from "../ReuseableComponents/Button";
-import {useState} from "react";
+import { useState, useEffect } from "react";
+import { getAllItems, getAllItemsInStock } from "../API";
+import { formatDateToReadable } from "../Inventory/index";
 
 const FridgeWrapper = styled.div`
   display: flex;
@@ -16,8 +18,6 @@ const Title = styled.h1`
   font-size: 2.5rem;
   color: white;
 `;
-
-
 
 const SubTitle = styled.h4`
   margin: 0;
@@ -47,25 +47,25 @@ const Td = styled.td`
   border: 1px solid #ccc;
   padding: 10px;
   text-align: center;
-`
+`;
 
 const ButtonWrapper = styled.div`
-display: flex;
-justify-content: center;
-`
+  display: flex;
+  justify-content: center;
+`;
 const Overlay = styled.div`
-  display: ${props => (props.overlayState ? 'block' : 'none')};
+  display: ${(props) => (props.overlayState ? "block" : "none")};
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.5); 
-  z-index: 998; 
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 998;
 `;
 
 const AddDiv = styled.div`
-  display: ${props => props.addButtonState ? 'flex' : 'none'};
+  display: ${(props) => (props.addButtonState ? "flex" : "none")};
   flex-direction: column;
   width: 600px;
   height: 400px;
@@ -76,12 +76,10 @@ const AddDiv = styled.div`
   z-index: 999;
   border-radius: 10px;
   padding: 15px;
-
-  
 `;
 
 const RemoveDiv = styled.div`
-  display: ${props => props.removeButtonState ? 'flex' : 'none'};
+  display: ${(props) => (props.removeButtonState ? "flex" : "none")};
   flex-direction: column;
   width: 600px;
   height: 400px;
@@ -92,21 +90,14 @@ const RemoveDiv = styled.div`
   z-index: 999;
   border-radius: 10px;
   padding: 15px;
-
-  
 `;
 
-
-
 const Form = styled.form`
-  
-margin-top: 10px;
-align-self: center;
-width: 300px;
+  margin-top: 10px;
+  align-self: center;
+  width: 300px;
 
-
-
-label {
+  label {
     color: white;
     display: block;
     margin-bottom: 10px;
@@ -127,7 +118,6 @@ label {
     border-color: #00bcd4; /* Change the border color when the input is in focus */
   }
 `;
-
 
 function ItemForm() {
   const [itemName, setItemName] = useState("");
@@ -164,40 +154,49 @@ function ItemForm() {
   );
 }
 
-
-//Add Items Pop Up 
+//Add Items Pop Up
 const AddComponent = ({ addButtonState, AddButton, overlayState }) => (
-    
-    <>
+  <>
     <Overlay overlayState={overlayState}></Overlay>
     <AddDiv addButtonState={addButtonState}>
-        <Button alignSelf={"flex-end"} width={"90px"} onClick={AddButton} >Close</Button>
-        <ItemForm/>
-        <Button alignSelf={"center"} width={"140px"} backgroundcolor={"#61ff69"}>Add Item</Button>
+      <Button alignSelf={"flex-end"} width={"90px"} onClick={AddButton}>
+        Close
+      </Button>
+      <ItemForm />
+      <Button alignSelf={"center"} width={"140px"} backgroundcolor={"#61ff69"}>
+        Add Item
+      </Button>
     </AddDiv>
-    </>
-    
+  </>
 );
 
 // Remove Items
-const RemoveComponent = ({ removeButtonState, RemoveButton , overlayState }) => (
-    
+const RemoveComponent = ({ removeButtonState, RemoveButton, overlayState }) => (
   <>
-  <Overlay overlayState={overlayState}></Overlay>
-  <RemoveDiv removeButtonState={removeButtonState}>
-      <Button alignSelf={"flex-end"} width={"90px"} onClick={RemoveButton} >Close</Button>
-      <Button alignSelf={"center"} width={"140px"} backgroundcolor={"#ff6961"}>Remove Item</Button>
-  </RemoveDiv>
+    <Overlay overlayState={overlayState}></Overlay>
+    <RemoveDiv removeButtonState={removeButtonState}>
+      <Button alignSelf={"flex-end"} width={"90px"} onClick={RemoveButton}>
+        Close
+      </Button>
+      <Button alignSelf={"center"} width={"140px"} backgroundcolor={"#ff6961"}>
+        Remove Item
+      </Button>
+    </RemoveDiv>
   </>
-  
 );
-
-
 
 export function Fridge() {
   const [addButtonState, setState] = useState(false);
   const [removeButtonState, setRemoveState] = useState(false);
   const [overlayState, setOverlay] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    // Fetch data when the component mounts
+    getAllItemsInStock()
+      .then((data) => setItems(data))
+      .catch((error) => console.error(error));
+  }, []);
 
   function AddButton() {
     setState(!addButtonState);
@@ -209,34 +208,56 @@ export function Fridge() {
     setOverlay(!overlayState);
   }
 
-
   return (
     <FridgeWrapper>
       <Title>Access Fridge</Title>
       <SubTitle>Manage Items in Fridge</SubTitle>
-      <AddComponent addButtonState={addButtonState} AddButton={AddButton} overlayState={overlayState} />
-      <RemoveComponent removeButtonState={removeButtonState} RemoveButton={RemoveButton}></RemoveComponent>
+      <AddComponent
+        addButtonState={addButtonState}
+        AddButton={AddButton}
+        overlayState={overlayState}
+      />
+      <RemoveComponent
+        removeButtonState={removeButtonState}
+        RemoveButton={RemoveButton}
+      ></RemoveComponent>
       <TableWrapper>
         <Table>
           <thead>
             <tr>
-              <Td>Name</Td>
-              <Td>Quantity</Td>
-              <Td>Expiry</Td>
+              <th>Product Name</th>
+              <th>Quantity</th>
+              <th>Expiry Date</th>
+              <th>Product Price</th>
+              <th>Last Updated</th>
+              {/* Add more table headers based on your data structure */}
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <Td>John Smith</Td>
-              <Td>26/2/12</Td>
-              <Td>38</Td>
-            </tr>
+            {items.map((item) => (
+              <tr key={item.productID}>
+                <Td>{item.Name}</Td>
+                <Td>{item.quantity}</Td>
+                <Td>{formatDateToReadable(item.expiryDate)}</Td>
+                <Td>{item.Price}</Td>
+                <Td>{formatDateToReadable(item.lastUpdated)}</Td>
+                {/* Add more table cells based on your data structure */}
+              </tr>
+            ))}
           </tbody>
         </Table>
       </TableWrapper>
       <ButtonWrapper>
-        <Button width={"140px"} backgroundcolor={"#61ff69"} onClick={AddButton}>Add Item</Button>
-        <Button width={"140px"} backgroundcolor={"#ff6961"} onClick={RemoveButton}>Remove Item</Button>
+        <Button width={"140px"} backgroundcolor={"#61ff69"} onClick={AddButton}>
+          Add Item
+        </Button>
+        <Button
+          width={"140px"}
+          backgroundcolor={"#ff6961"}
+          onClick={RemoveButton}
+        >
+          Remove Item
+        </Button>
       </ButtonWrapper>
     </FridgeWrapper>
   );
